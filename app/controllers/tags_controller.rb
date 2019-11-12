d3c9e15a19d14bdb48a8d8264b3a4077172bd1ac
Zslw <dan.juarez192000@gmail.com>
2019-11-12 17:34:37 -0600

    Tagging feature completed

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,15 @@
 class TagsController < ApplicationController
     def index
-        @tag = Tag.all
+        @tags = Tag.all
     end
     def show
         @tag = Tag.find(params[:id])
+    end
+    def destroy
+        @tag = Tag.destroy(params[:id])
+
+        flash.notice = "Tag: '#{@tag.name}' Deleted!"
+
+        redirect_to tags_path() 
     end
 end
